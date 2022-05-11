e40104a3c949802a5e0d8c6b372c8db221989704
Andrew Glassman <andrew.glassman@gmail.com>
2022-05-11 17:14:44 -0500

    Initial bezier curve with control.

--- demo_1/main.lua
+++ demo_1/main.lua
@@ -1,35 +1,39 @@
-points={p0={x=0,y=0}, p1={x=40,y=80}, p2={x=80,y=80}}
+points={{x=0,y=0},{x=40,y=80},{x=80,y=80}}
+
+sel = 0
 
 function _init()
 
 end	
 
 function _update()
-	if (btn(⬅️)) points.p1.x -= 1
-	if (btn(➡️)) points.p1.x += 1
-	if (btn(⬆️)) points.p1.y -= 1
-	if (btn(⬇️)) points.p1.y += 1	
+	if (btnp(❎)) sel +=1
+	if (btnp(❎)) sel %=3
+	if (btn(⬅️)) points[sel + 1].x -= 1
+	if (btn(➡️)) points[sel + 1].x += 1
+	if (btn(⬆️)) points[sel + 1].y -= 1
+	if (btn(⬇️)) points[sel + 1].y += 1	
 end
 
 function _draw()
 	cls()
-	for k, point in pairs(points) do
-		draw_point(point)
-	end	
 	draw_bezier(points)
+	foreach(points, draw_point)
+	draw_point(points[sel + 1], 3)
 end	
 
-function draw_point(point)
-	pset(point.x, point.y, 3)
-end	
+function draw_point(p, c)
+	c = c or 2
+	pset(p.x, p.y, c)
+end
 
-function draw_bezier(points)
-	n = abs(points.p2.x - points.p0.x)
+function draw_bezier(b)
+	n = max(abs(b[3].x - b[1].x),50)
 	for i=0, n, 1 do
 		t= i/n
-		lx = quad_bezier(t, points.p0.x, points.p1.x, points.p2.x)
-		ly = quad_bezier(t, points.p0.y, points.p1.y, points.p2.y)
-		pset(lx, ly, 4)
+		lx = quad_bezier(t, b[1].x, b[2].x, b[3].x)
+		ly = quad_bezier(t, b[1].y, b[2].y, b[3].y)
+		pset(lx, ly, 6)
 	end	
 end	
 
