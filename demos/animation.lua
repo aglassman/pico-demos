5194fbf80d3c48559addc05eeb51e43e804862be
Andrew Glassman <andrew.glassman@gmail.com>
2022-05-18 22:56:25 -0500

    Full animation demo

--- demos/animation.lua
+++ demos/animation.lua
@@ -1,7 +1,79 @@
--- Always return the object's position
-function a_identity(game_obj)
-	return game_obj.position[1], game_obj.position[2]
-end
+--[[
+Pico Animation
+
+A 2D animation library
+
+
+First, define an animation.
+Animations are declarative, and are stored as tables.  You can reuse them, or clone them. They don't hold any
+animation state.
+
+You can start simple, and expand on it.
+
+This example will animate the object in a circle, once, over one second.
+
+```
+small_circle = {
+	pipeline={circle(20,20)}
+}
+```
+
+This will loop the animatio 10 times, each iteration will take 3 seconds.
+
+```
+small_circle_inf = {
+	pipeline={circle(20,20)},
+	duration=3,
+	loop=100
+}
+```
+
+You can declare multiple functions to effect transform pipeline.
+You can also infinitely loop
+
+```
+small_circle_inf = {
+	pipeline={circle(20,20), linear(20, 0)},
+	duration=3,
+	loop=-1
+}
+
+```
+
+You can animate any object that is represented as a table, it only needs a `position` entry, which stores {x, y} coordinates.
+
+```
+target = {
+	position={64, 64}
+}
+
+animate(target, small_circle_inf)
+
+```
+
+The animation will then attach transpose functions, and state to the target.
+See animate function documentation for details.
+
+When the animation is complete, target.animation_complete will be true.
+
+You can halt an animation using:
+
+```
+halt_animation(target)
+```
+
+This will set target.animation_complete == true, and change the transpose function back to the identiy function.
+
+
+]]
+
+
+--[[
+Pipeline Transpose Functions
+
+These functions can be composed to combine any number of individual transpose functions into one
+function.
+]]
 
 --[[
 	Returns a new quadratic bezier function with n resolution. pct=0..1
@@ -13,99 +85,143 @@ function q_bezier(b)
 	end
 end	
 
--- linear interpolation (start, end, percent)
-function lerp(a, b, c)
-	return a + (b - a) * c
-end
+function translate(x, y)
+	return function(pct)
+		return x, y
+	end
+end	
 
-function qb(t, p0, p1, p2)
-	return lerp(lerp(p0, p1, t), lerp(p1, p2, t), t)
-end
-
-function linear(pct, point)
-	return pct * point.x, pct * point.y
+function linear(x, y)
+	return function(pct)
+		return pct * x, pct * y
+	end	
 end	
 
 
-function transpose(fn)
-	return function(obj, pct)
-		local dx, dy = fn(pct)
-		return obj.position[1] + dx, obj.position[2] + dy
+function sin_x(multiplier)
+	return function(pct)
+		return sin(pct) * multiplier, 0
+	end 
+end
+
+function sin_y(multiplier)
+	return function(pct)
+		return 0, sin(pct) * multiplier
+	end 
+end	
+
+
+function sin_xy(x_multiplier, y_multiplier)
+	return function(pct)
+		return sin(pct) * x_multiplier, sin(pct) * y_multiplier
 	end
 end
+
+function circle(x_multiplier, y_multiplier)
+return function(pct)
+		return sin(pct) * x_multiplier, cos(pct) * y_multiplier
+	end
+end	
+
+
+function create_pipeline(pipeline)
+	return function(self, pct)
+		local x = self.position[1]
+		local y = self.position[2]
+		for fn in all(pipeline) do
+			local dx, dy = fn(pct)
+			printh("d: " .. dx .. ", " .. dy, "demo_log.txt")
+			x += dx
+			y += dy
+		end
+		printh("new: " .. x .. ", " .. y, "demo_log.txt")
+		return x, y	
+	end
+end	
+
+
+-- Always return the object's position
+function a_identity(game_obj)
+	return game_obj.position[1], game_obj.position[2]
+end
+
 
 --[[
 	animate(target, args)
 	target - a target table to anmiate.
-	* Requires an entry of "position" of shape {x=number, y=number}
+	* Requires an entry of "position" of shape {x, y} where x and y are of type number.
 	
 	Animate stores the following attributes on the target:
-	* st: number (start time)
-	* et: number (end time)
-	* loop_count: number (to track iterations)
-	* reverse: boolean (to track direction)
+	* animation_st: number (start time)
+	* animation_et: number (end time)
+	* animation_loop_count: number (to track iterations)
+	* animation_reverse: boolean (to track direction)
 	* animation_complete: boolean (to track if iteration is complete)
-	* transpose_fn: Function that returns an x/y coordinate for where the sprite should be drawn.
-	* draw_path_fn: Function that can draw the full animation path.
+	* animation_transpose_fn: Function that returns an x/y coordinate for where the sprite should be drawn.
+	* animation_draw_path_fn: Function that can draw the full animation path.
 
 	Animate Args
 	* duration: in seconds, the length of the animation (default 1.0)
 	* loop:  Number of iterations (default 1)
 	* direction:  "forward" or "reverse" (default "forward")
 	* mirror: boolean (default false) Will mirror animation after each iteration.
-	* smooth: If animate is called for the target object before a previous animation
+	* transition: string (default nil). Effects the way an in-progress animation will be handled if a new
+		animation is applied to the same target.
+	   * "smooth" - If animate is called for the target object before a previous animation
 		is complete, it will not reset reverse, loop_count, or st attributes. It will
 		only recalcuate the end time, and replace the transpose_fn, and path attributes.
+	* offset: offset the animation (+/- 0..1)
 ]]
 function animate(target, args)
 	local duration = args.duration or 1
 	local loop = args.loop or 1
 	local direction = args.direction or "forward"
 	local mirror = args.mirror or false
+	local offset = args.offset or 0
 
 
 	if (not target.animation_complete) and args.transition == "smooth" then
-		target.et = target.st + duration
+		target.animation_et = target.animation_st + duration
 	else
 		if direction == "forward" then
-			target.reverse = false
+			target.animation_reverse = false
 		else
-			target.reverse = true
+			target.animation_reverse = true
 		end	
 		
-		target.st = time()
-		target.et = time() + duration
-		target.loop_count = 0
+		target.animation_st = time()
+		target.animation_et = time() + duration
+		target.animation_loop_count = 0
 
 	end
 	
 	target.animation_complete = false
-	target.draw_path_fn = args.draw_path_fn
+	target.animation_draw_path_fn = args.draw_path_fn
 
-	target.transpose_fn = function (self)
+	local pipelin_fn = create_pipeline(args.pipeline)
+
+	target.animation_transpose_fn = function (self)
 		local pct
+		local t = time() + offset
 		if self.reverse then
-			pct = 1 - (( time() - self.st) / duration)
+			pct = 1 - (( t - self.animation_st) / duration)
 		else
-			pct = ( time() - self.st) / duration
+			pct = ( t - self.animation_st) / duration
 		end	
 
-		local x, y = args.transpose_fn(self, pct)
+		local x, y = pipelin_fn(self, pct)
 
-		if time() > self.et then
-			if self.loop_count == loop - 1 then
-				local x, y = args.transpose_fn(self, 1)
-				self.position = {x, y}
-				self.transpose_fn = a_identity
-				self.animation_complete = true
+		if time() > self.animation_et then
+			if self.animation_loop_count == loop - 1 then
+				halt_animation(self)
 				return x, y
 			else
 				if mirror then
-					self.reverse = not self.reverse
+					self.animation_reverse = not self.animation_reverse
 				end	
-				self.st = time()
-				self.et = time() + duration
-				self.loop_count += 1
+				self.animation_st = time()
+				self.animation_et = time() + duration
+				self.animation_loop_count += 1
 			end	
 		end	
 
@@ -113,3 +229,18 @@ function animate(target, args)
 	end
 end
 
+function halt_animation(target)
+	target.animation_transpose_fn = a_identity
+	target.animation_complete = true
+end	
+
+-- Supporting Functions
+-- linear interpolation (start, end, percent)
+function lerp(a, b, c)
+	return a + (b - a) * c
+end
+
+-- quadratic bezier
+function qb(t, p0, p1, p2)
+	return lerp(lerp(p0, p1, t), lerp(p1, p2, t), t)
+end
--- demos/demo_1.lua
+++ demos/demo_1.lua
@@ -1,128 +1,295 @@
--- 128 x 128
-quad_bezier_points={{x=0,y=0},{x=40,y=80},{x=40,y=80}}
-
-sel = 0
-
-obj = {
-	text="funky butt lovin",
+target = {
 	sprite=1,
-	position={x=-4, y=-4}
+	position={64, 64}
 }
 
 objs = {}
 
-speed=1
+duration=1
 
-function _init()
-	-- animate(obj, {
-	-- 	transpose_fn=transpose(q_bezier(quad_bezier_points)),
-	-- 	draw_path_fn=q_bezier_draw(quad_bezier_points),
-	-- 	duration=speed, 
-	-- 	direction="reverse",
-	-- 	mirror=true,
-	-- 	loop=3})
+index = -1
 
-	local str = "hello world"
 
+function multi_offset_example()
+	local str="hello world"
+	local objs = {}
 	for i = 1, #str do
     	local c = sub(str, i,i)
-    	objs[i] = {
+    	local new_obj = {
 				text=c,
-				position={i * 5,-4}
+				text_color=11,
+				position={64, 64}
 			}
-    	animate(
-			objs[i],
-	    	{
-				transpose_fn=transpose(q_bezier(quad_bezier_points)),
-				draw_path_fn=q_bezier_draw(quad_bezier_points),
-				--duration=speed/(i*.8),
-				mirror=true,
-				loop=1})
+		local new_animation = {
+	    		offset=.3*i,
+				pipeline={circle(20,20)},
+				duration=5,
+				mirror=false,
+				loop=-1}	
+    	objs[i]={obj=new_obj, animation=new_animation}
 	end
-
-	-- animate(obj, {transpose_fn=transpose(q_bezier(quad_bezier_points))})
+	return objs
 end	
-
-function _update()
-	if (btnp(❎)) sel +=1
-	if (btnp(❎)) sel %=3
-	if (btn(⬅️)) quad_bezier_points[sel + 1].x -= 2
-	if (btn(➡️)) quad_bezier_points[sel + 1].x += 2
-	if (btn(⬆️)) quad_bezier_points[sel + 1].y -= 2
-	if (btn(⬇️)) quad_bezier_points[sel + 1].y += 2
-
-	if (btnp(🅾️)) then
-
-	local str = "hello world"
-
-	for i = 1, #str do
-    	local c = sub(str, i,i)
-    	objs[i] = {
-				text=c,
-				position={i * 5,-4}
-			}
-    	animate(
-			objs[i],
-	    	{
-				transpose_fn=transpose(q_bezier(quad_bezier_points)),
-				draw_path_fn=q_bezier_draw(quad_bezier_points),
-				duration=speed*(i*.2),
-				mirror=true,
-				loop=1})
-	end
-
-	end 
-
-	--if (btnp(❎)) speed +=.02; printh(speed, "demo_log.txt");
-	--if (btnp(⬅️)) animate(obj, {fn=linear, args={x= -30, y= 30}}, speed)
-	--if (btnp(➡️)) animate(obj, {fn=linear, args={x= 30, y= 30}}, speed, "mirror")
-	--if (btnp(⬆️)) animate(obj, {fn=linear, args={x= 0, y= -30}}, speed, "mirror")
-	--if (btnp(⬇️)) animate(obj, {fn=quad_bezier, args=q_bezier(points)}, speed, "mirror")
-
-end
-
-function _draw()
-	cls()
-	foreach(objs,draw_obj)
-	--pset(obj.position.x, obj.position.y,11)
-end	
-
-
-function draw_obj(obj)
-	local x, y = obj:transpose_fn()
-
-	if obj.sprite then
-		spr(obj.sprite, x, y)
-	end	
-
-	if obj.text then
-		print(obj.text, x, y, 11)
-	end	
-	
-	if obj.draw_path_fn then
-		obj:draw_path_fn()
-	end	
-end	
-
 
 function q_bezier_draw(points)
 	local q_bz = q_bezier(points)
 	return function()
 		n = 200
 		for i=0, n, 1 do
-			t = i / n
-			x, y = q_bz(t)
+			local t = i / n
+			local x, y = q_bz(t)
 			pset(x, y, 7)
 		end	
 
-		foreach(points, draw_point)
-		draw_point(points[sel + 1], 11)
+		for p in all(points) do
+			pset(p.x, p.y, 8)
+		end
 	end	
 end
 
+examples = {
+		{
+			description="linear - x only, 3 times",
+			obj=target,
+			animation={
+				pipeline={linear(50, 0)},
+				duration=duration,
+				loop=3
+			}
+		},
+		{
+			description="linear - y only, 3 times",
+			obj=target,
+			animation={
+				pipeline={linear(0, 50)},
+				duration=duration,
+				loop=3
+			}
+		},
+		{
+			description="linear - x and y, 3 times",
+			obj=target,
+			animation={
+				pipeline={linear(50, 50)},
+				duration=duration,
+				loop=3
+			}
+		},
+		{
+			description="linear - x only, reverse, 3 times",
+			obj=target,
+			animation={
+				pipeline={linear(0, 50)},
+				duration=duration,
+				direction="reverse",
+				loop=3
+			}
+		},
+		{
+			description="linear - x only, mirror, 3 times",
+			obj=target,
+			animation={
+				pipeline={linear(0, 50)},
+				duration=duration,
+				mirror=true,
+				loop=3
+			}
+		},
+		{
+			description="circle",
+			obj=target,
+			animation={
+				pipeline={circle(20, 20)},
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="oval",
+			obj=target,
+			animation={
+				pipeline={circle(50, 20)},
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="smooth transition (try change spped)",
+			obj=target,
+			animation={
+				pipeline={linear(30, 0)},
+				duration=duration,
+				transition="smooth",
+				mirror= true,
+				loop=-1
+			}
+		},
+		{
+			description="sin_x",
+			obj=target,
+			animation={
+				pipeline={sin_x(30)},
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="sin_y",
+			obj=target,
+			animation={
+				pipeline={sin_y(30)},
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="sin_x -> sin_y",
+			obj=target,
+			animation={
+				pipeline={sin_x(15), sin_y(30)},
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="quad bezier - 1",
+			obj=target,
+			animation = {
+				pipeline={translate(-64, -64),q_bezier({{x=0,y=64},{x=64,y=10},{x=128,y=64}})},
+				draw_path_fn=q_bezier_draw({{x=0,y=64},{x=64,y=10},{x=128,y=64}}),
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="quad bezier - 2",
+			obj=target,
+			animation = {
+				pipeline={translate(-64, -64),q_bezier({{x=30,y=100},{x=100,y=10},{x=100,y=100}})},
+				draw_path_fn=q_bezier_draw({{x=30,y=100},{x=100,y=10},{x=100,y=100}}),
+				duration=duration,
+				loop=-1
+			}
+		},
+		{
+			description="quad bezier - mirror",
+			obj=target,
+			animation = {
+				pipeline={translate(-64, -64),q_bezier({{x=30,y=100},{x=100,y=10},{x=100,y=100}})},
+				draw_path_fn=q_bezier_draw({{x=30,y=100},{x=100,y=10},{x=100,y=100}}),
+				duration=duration,
+				mirror=true,
+				loop=-1
+			}
+		},
+		{
+			description="quad bezier - no path",
+			obj=target,
+			animation = {
+				pipeline={translate(-64, -64),q_bezier({{x=30,y=100},{x=100,y=10},{x=100,y=100}})},
+				duration=duration,
+				mirror=true,
+				loop=-1
+			}
+		},
+		{
+			description="text",
+			obj={
+				text="hello world",
+				text_color=11,
+				position={64, 64}
+			},
+			animation={
+				pipeline={circle(20,20)},
+				duration=duration,
+				mirror=false,
+				loop=-1}
+		},
+		{
+			description="offset",
+			multi=multi_offset_example()
+		}						
+}
 
-function draw_point(p, c)
-	c = c or 8
-	pset(p.x, p.y, c)
+for i=1, #examples do
+	examples[i].index = i
+end	
+
+
+function get_example()
+	return examples[(index % #examples) + 1]
+end	
+
+function reset_example()
+	local example = get_example()
+
+	if example.multi then
+		halt_animation(target)
+		target.hide = true
+		for e in all(example.multi) do
+			e.animation.duration = 3.6
+			animate(e.obj, e.animation)
+		end
+	else
+		target.hide = false
+		example.animation.duration = duration
+		animate(example.obj, example.animation)
+	end	
+end	
+
+function next_example()
+	index += 1
+	reset_example()
 end
 
+function previous_example()
+	index -= 1
+	reset_example()
+end		
+
+function _init()
+	next_example()
+end	
+
+function _update()
+	if (btnp(❎)) sel +=1
+	if (btnp(❎)) sel %=3
+	if (btnp(⬅️)) previous_example()
+	if (btnp(➡️)) next_example()
+	if (btnp(⬆️)) duration += 0.2; reset_example()
+	if (btnp(⬇️)) duration -= 0.2; reset_example()	
+end
+
+function _draw()
+	cls()
+	example = get_example()
+	print(example.index .. ": " .. example.description, 10, 10, 7)
+	print("duration: " .. duration, 10, 20, 7)
+	if example.multi then
+		for e in all(example.multi) do
+			draw_obj(e.obj)
+		end
+	else
+		draw_obj(example.obj)
+	end	
+end	
+
+
+function draw_obj(obj)
+	local x, y = obj:animation_transpose_fn()
+
+	if not obj.hide then
+
+		if obj.sprite then
+			spr(obj.sprite, x, y)
+		end	
+
+		if obj.text then
+			print(obj.text, x, y, obj.text_color)
+		end	
+		
+		if obj.animation_draw_path_fn then
+			obj:animation_draw_path_fn()
+		end
+	end		
+end	
