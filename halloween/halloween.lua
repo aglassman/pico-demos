4773221cb35464b0baf1583907f1b07f5bf7a04f
Andrew Glassman <andrew.glassman@gmail.com>
2022-10-31 16:42:21 -0500

    Finished Checkers

--- halloween/halloween.lua
+++ halloween/halloween.lua
@@ -1,3 +1,5 @@
+winner = nil
+
 player=0
 
 score={}
@@ -37,11 +39,11 @@ selection = {
 	set_selection=function(self, piece, x, y)
 		piece:set_select(true)
 		self.current={x=x, y=y}
-		self.valid_moves= valid_moves(player, x, y)
+		self.valid_moves= valid_moves(player, x, y, piece.king)
 	end,
 
 	clear_selection=function(self, piece)
-		piece:set_select(falsec)
+		piece:set_select(false)
 		self.current=nil
 		self.valid_moves={}
 	end,
@@ -64,6 +66,7 @@ selection = {
 		dp("player " .. player)
 		dp("from {" .. self.current.x .. ", " .. self.current.y ..  "}")
 		dp("to {" .. move.x .. ", " .. move.y ..  "}")
+		
 		local piece = get_position(self.current.x, self.current.y)
 		set_position(self.current.x, self.current.y, nil)
 		set_position(move.x, move.y, piece)
@@ -71,11 +74,21 @@ selection = {
 
 		if move.capture then
 			dp("capture {" .. move.capture.x .. ", " .. move.capture.y ..  "}")
+		
 			set_position(move.capture.x, move.capture.y, nil)
 			score[player] = (score[player] or 0) + 1
+		
+			dp("----")
+		end
+		
+		if (player == 0 and move.y == 7) or (player == 1 and move.y == 0) then
+			piece.king = true
 		end	
+
 		dp("----")
-		next_player()
+		
+		next_player(piece, move)
+			
 	end,
 
 	draw=function(self)
@@ -94,78 +107,6 @@ selection = {
 
 
 }
---printh("{" .. position[1] .. ", " .. position[2] .. "}", "demo_log.txt")
-function dp(string)
-	printh( string, "halloween.txt")
-end	
-
-function next_player()
-	player = (player + 1) % 2
-end	
-
-function draw_rect(x, y, color, fill)
-	if fill then
-		rectfill(x * 16, y * 16, x * 16 + 16, y * 16 + 16, color)
-	else
-		rect(x * 16, y * 16, x * 16 + 15, y * 16 + 15, color)
-	end	
-end
-
-function draw_sprite(sprite, x, y)
-	spr(sprite, x * 16, y * 16, 2, 2)
-end
-
-function get_position(x, y)
-	if x < 0 or x > 7 or y < 0 or y > 7 then
-		return nil
-	else
-		return positions[x][y]
-	end	
-end	
-
-function set_position(x, y, piece)
-	positions[x][y] = piece
-end	
-
-function valid_moves(player, x, y, capture)
-	capture = capture or false
-	local direction
-
-	if player == 0 then
-		direction = 1
-	else
-		direction = -1
-	end
-
-	y += direction
-	
-	local moves = {}
-	local option_1 = get_position(x-1, y)
-	local option_2 = get_position(x+1, y)
-
-
-	if (capture and capture.opt == 2) then
-		--nothing
-	elseif option_1 == nil then
-		add(moves, {x=x-1, y=y, capture=capture})
-	elseif option_1.player != player then
-		for move in all(valid_moves(player, x-1, y, {x=x-1, y=y, opt=1})) do
-			add(moves, move)
-		end	
-	end	
-	
-	if (capture and capture.opt == 1) then
-		--nothing
-	elseif option_2 == nil then
-		add(moves, {x=x+1, y=y, capture=capture})
-	elseif option_2.player != player then
-		for move in all(valid_moves(player, x+1, y, {x=x+1, y=y, opt=2})) do
-			add(moves, move)
-		end	
-	end	
-
-	return moves
-end
 
 piece = {
 	player=0,
@@ -183,13 +124,21 @@ piece = {
 	end,
 
 	draw=function(self, x, y)
+		if self.king then
+			pal(0, 8)
+		end	
+
 		if self.selected then
 			local offset = flr(time() * self.sprite.speed % #self.sprite.selected) + 1
 			local selected_sprite = self.sprite.selected[offset]
 			draw_sprite(selected_sprite, x, y)
 		else
 			draw_sprite(self.sprite.unselected, x, y)
-		end		
+		end	
+
+		if self.king then
+			pal(0, 0)
+		end	
 	end,
 
 	set_select=function(self, selected)
@@ -202,25 +151,33 @@ selector = {
 	position={x=0,y=0},
 	colors={8,7},
 	draw=function(self)
+		local color
+
+		if player == 0 then
+			color = 10
+		elseif player == 1 then
+			color = 7
+		end	
+
 		if (self.position != nil) then
-			draw_rect(self.position.x, self.position.y, 7, false)
+			draw_rect(self.position.x, self.position.y, color, false)
 		end
 	end,
 
 	update=function(self)
-		if (btnp(⬅️, player)) then
+		if (btnp(⬅️, player)) and in_bounds(self.position.x - 1) then
 			self.position.x -= 1
 		end
 		 
-		if (btnp(➡️, player)) then
+		if (btnp(➡️, player)) and in_bounds(self.position.x + 1) then
 			self.position.x += 1
 		end
 			
-		if (btnp(⬆️, player)) then
+		if (btnp(⬆️, player)) and in_bounds(self.position.y - 1) then
 			self.position.y -= 1
 		end
 			
-		if (btnp(⬇️, player)) then
+		if (btnp(⬇️, player)) and in_bounds(self.position.y + 1) then
 			self.position.y += 1
 		end
 
@@ -278,12 +235,128 @@ function _init()
 end	
 
 function _update()
-	selector:update()
+	if not winner then
+		selector:update()
+		check_win()
+	end		
 end
 
 function _draw()
-	cls()
-	board:draw()
-	selector:draw()
-	selection:draw()
+	
+	if winner then
+		cls()
+		board:draw()
+		draw_winner()
+	else
+		cls()
+		board:draw()
+		selector:draw()
+		selection:draw()
+	end	
 end	
+
+function draw_winner()
+	rectfill(8, 40, 120, 75, 0)
+	local winner_sprite
+	if winner == "pumpkins" then
+		winner_sprite = piece:new(0, pumpkin)
+	else
+		winner_sprite = piece:new(0, ghost)
+	end	
+		
+	winner_sprite.selected = true
+	winner_sprite:draw(1, 3)
+	winner_sprite:draw(6, 3)
+
+	print(winner .. " win!", 40, 55, 7)
+end	
+
+function check_win()
+	if (score[0] or 0) == 12 then
+		winner = "pumpkins"
+	end
+	
+	if (score[1] or 0) == 12 then
+		winner = "ghosts"
+	end
+end	
+
+function dp(string)
+	printh( string, "halloween.txt")
+end	
+
+function next_player(piece, move)
+	if piece and move.capture then
+		for next_move in all(valid_moves(player, move.x, move.y, piece.king)) do
+			if next_move.capture then
+				dp("double_jump_possible {" .. next_move.capture.x .. ", " .. next_move.capture.y ..  "}")
+				return false
+			end	
+		end	
+	end
+	dp("next player")
+	player = (player + 1) % 2
+end	
+
+function draw_rect(x, y, color, fill)
+	if fill then
+		rectfill(x * 16, y * 16, x * 16 + 16, y * 16 + 16, color)
+	else
+		rect(x * 16, y * 16, x * 16 + 15, y * 16 + 15, color)
+	end	
+end
+
+function draw_sprite(sprite, x, y)
+	spr(sprite, x * 16, y * 16, 2, 2)
+end
+
+function in_bounds(a)
+	return a >= 0 and a <= 7
+end	
+
+function get_position(x, y)
+	if in_bounds(x) and in_bounds(y) then
+		return positions[x][y]
+	else
+		return nil
+	end	
+end	
+
+function set_position(x, y, piece)
+	positions[x][y] = piece
+end	
+
+function valid_moves(player, x, y, is_king)
+	local directions
+
+	if is_king then
+		directions = {-1, 1}
+	else
+		if player == 0 then
+			directions = {1}
+		else
+			directions = {-1}
+		end
+	end
+
+	x_options = {-1, 1}
+
+	local moves = {}
+
+	for direction in all(directions) do
+		local y += direction
+		for x_option in all(x_options) do
+			local potential_move = get_position(x + x_option, y)
+			if potential_move == nil and in_bounds(x + x_option) and in_bounds(y) then
+				add(moves, {x=x + x_option, y=y})
+			elseif potential_move != nil and potential_move.player != player then
+				potential_move = get_position(x + (x_option * 2), (y + direction))
+				if potential_move == nil and in_bounds(x + (x_option * 2)) and in_bounds(y + direction) then
+					add(moves, {x=x + (x_option * 2), y=(y + direction), capture={x=x + x_option, y=y}})
+				end	
+			end	
+		end
+	end		
+
+	return moves
+end
